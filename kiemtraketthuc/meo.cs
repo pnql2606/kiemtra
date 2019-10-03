66699b9899e219d3bd11fd4b3b732bc74f18a31d
Pham Nguyen Quang Luu <xinemyeu2254@gmail.com>
2019-10-03 15:12:31 +0700

    all

--- kiemtraketthuc/meo.cs
+++ kiemtraketthuc/meo.cs
@@ -8,5 +8,10 @@ namespace kiemtraketthuc
 {
     class meo
     {
-    }
+        class Meo : Dongvat
+        {
+            public override void Talk()
+            {
+                base.Talk();
+            }
 }
