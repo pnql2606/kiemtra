e67c4e4b75fb9fd0b54851909e7d4acc4fb01cb7
Pham Nguyen Quang Luu <xinemyeu2254@gmail.com>
2019-10-03 15:07:43 +0700

    all

---
+++ kiemtraketthuc/dongvat.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiemtraketthuc
+{
+    class dongvat
+    {
+        private string Keu;
+
+        public string DongvatKeu
+        {
+            get
+            {
+                return Keu;
+            }
+            set
+            {
+                Keu = value;
+            }
+        }
+
+        public Dongvat()
+        {
+        }
+
+        private string MauLong;
+
+        public string DongvatMauLong
+        {
+            get
+            {
+                return MauLong;
+            }
+            set
+            {
+                MauLong = value;
+            }
+        }
+
+
+
+        public virtual void Talk()
+        {
+            if (DongvatKeu.Equals("Gâu gâu") && (DongvatMauLong.Equals("Đỏ")))
+                MessageBox.Show(this.DongvatKeu + " Tôi là chó lông màu:" + this.DongvatMauLong);
+            else if (DongvatKeu.Equals("Meo meo") && (DongvatMauLong.Equals("Hồng")))
+                MessageBox.Show(this.DongvatKeu + " Tôi là mèo lông màu:" + this.DongvatMauLong);
+        }
+    }
+}
+    }
+
+}
