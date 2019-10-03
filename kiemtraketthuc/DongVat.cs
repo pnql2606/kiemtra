0f605aae257afc8af36f4afe5e981adcdda4bad6
pnql2606 <56054600+pnql2606@users.noreply.github.com>
2019-10-03 16:16:01 +0700

    Update and rename dongvat.cs to DongVat.cs

--- kiemtraketthuc/DongVat.cs
+++ kiemtraketthuc/DongVat.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace kiemtraketthuc
+namespace kiemtraketthucmon
 {
     class DongVat
     {
@@ -24,4 +24,3 @@ namespace kiemtraketthuc
         }
     }
 }
-
